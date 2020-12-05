2808f728ddefe832c13c73570dd42128e51f3c86
davidnac <58204061+davidnac@users.noreply.github.com>
2020-12-05 20:37:28 +0200

    Trial version

---
+++ js/scripts.js
@@ -0,0 +1,210 @@
+
+
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#sideNav",
+    });
+})(jQuery); // End of use strict
+
+  
+
+///////////items filter code/////////////
+$(document).ready(function(){
+    $(".list").click(function(){
+      const value = $(this).attr("data-filter");
+  
+      //1000 is msec for animation
+      if(value == "All"){
+        $(".itemBox").show("1000");
+      }else{
+        //hides all elements that dont have class .fillter
+          $(".itemBox").not("."+value).hide("1000");
+          //shows all elements that have class .fillter
+          $(".itemBox").filter("."+value).show("1000");
+      }
+    })
+      $(".list").click(function(){
+        $(this).addClass("active").siblings().removeClass("active");
+      })
+  })
+  ///////////items filter code/////////////
+
+  function showDiveLog(){
+    const title = "Dive log";
+    const logo = "images/buy4baby-1.png";
+    const bio = "ffffffffffffffffff";
+    const technologies = "Sqlite, Firebase: Auth,storage, realtime database ++, Async";
+    const mainImage = "images/galaxy.jpg";
+    const link = "https://play.google.com/store/apps/details?id=com.david.divelog";
+
+    showModal(title,bio,link,mainImage,technologies,logo);
+  }
+
+  function showDivePlanner(){
+    const title = "Dive planner";
+    const logo = "images/buy4baby-1.png";
+    const bio = "ffffffffffffffffff";
+    const technologies = "Sqlite, Firebase: Auth,storage, realtime database ++, Async";
+    const mainImage = "images/galaxy.jpg";
+    const link = "https://play.google.com/store/apps/details?id=com.davidnac.diveplanner";
+
+    showModal(title,bio,link,mainImage,technologies,logo);
+  }
+
+  function showTtsReader(){
+    const title = "TTS Reader";
+    const logo = "images/buy4baby-1.png";
+    const bio = "ffffffffffffffffff";
+    const technologies = "Sqlite, Firebase: Auth,storage, realtime database ++, Async";
+    const mainImage = "images/galaxy.jpg";
+    const link = "https://play.google.com/store/apps/details?id=com.davidnac.ttsreaderfree";
+
+    showModal(title,bio,link,mainImage,technologies,logo);
+  }
+
+  function showBarcodeReader(){
+    const title = "Barcode reader";
+    const logo = "images/buy4baby-1.png";
+    const bio = "ffffffffffffffffff";
+    const technologies = "Sqlite, Firebase: Auth,storage, realtime database ++, Async";
+    const mainImage = "images/galaxy.jpg";
+    const link = "https://play.google.com/store/apps/details?id=com.davidnac.barcodereader";
+
+    showModal(title,bio,link,mainImage,technologies,logo);
+  }
+
+  function showMorseTranslator(){
+    const title = "Morse Translator(";
+    const logo = "images/buy4baby-1.png";
+    const bio = "ffffffffffffffffff";
+    const technologies = "Sqlite, Firebase: Auth,storage, realtime database ++, Async";
+    const mainImage = "images/galaxy.jpg";
+    const link = "https://play.google.com/store/apps/details?id=com.david.morsetranslator";
+
+    showModal(title,bio,link,mainImage,technologies,logo);
+  }
+
+  
+  function showBlog(){
+    const title = "Morse Translator";
+    const logo = "images/buy4baby-1.png";
+    const bio = "ffffffffffffffffff";
+    const technologies = "Sqlite, Firebase: Auth,storage, realtime database ++, Async";
+    const mainImage = "images/galaxy.jpg";
+    const link = "NA";
+
+    showModal(title,bio,link,mainImage,technologies,logo);
+  }
+
+  function showAccellix(){
+    const title = "Accellix";
+    const logo = "images/buy4baby-1.png";
+    const bio = "ffffffffffffffffff";
+    const technologies = "Sqlite, Firebase: Auth,storage, realtime database ++, Async";
+    const mainImage = "images/galaxy.jpg";
+    const link = "NA";
+
+    showModal(title,bio,link,mainImage,technologies,logo);
+  }
+
+  function showBuy4babyAndroid(){
+    const title = "Buy4baby";
+    const logo = "images/buy4baby-1.png";
+    const bio = "ffffffffffffffffff";
+    const technologies = "Sqlite, Firebase: Auth,storage, realtime database ++, Async";
+    const mainImage = "images/galaxy.jpg";
+    const link = "https://play.google.com/store/apps/details?id=com.david.mybaby";
+
+    showModal(title,bio,link,mainImage,technologies,logo);
+  }
+
+  function showBuy4babyIos(){
+    const title = "Buy4baby";
+    const logo = "images/buy4baby-1.png";
+    const bio = "ffffffffffffffffff";
+    const technologies = "Sqlite, Firebase: Auth,storage, realtime database ++, Async";
+    const mainImage = "images/galaxy.jpg";
+    const link = "https://apps.apple.com/us/app/buy4baby/id1474004620";
+
+    showModal(title,bio,link,mainImage,technologies,logo);
+  }
+
+  function showBuy4babyWeb(){
+    const title = "Buy4baby";
+    const logo = "images/buy4baby-1.png";
+    const bio = "ffffffffffffffffff";
+    const technologies = "Sqlite, Firebase: Auth,storage, realtime database ++, Async";
+    const mainImage = "images/galaxy.jpg";
+    const link = "www.buy4baby.co.il";
+
+    showModal(title,bio,link,mainImage,technologies,logo);
+  }
+
+  function showModal(title,bio,link,mainImage,technologies,logo){
+    
+    // set title and body of alert
+      $('#appModal .modal-logo').attr("src",logo);;
+      $('#appModal .modal-mainImage').attr("src",mainImage);
+      $('#appModal .modal-title').text(title);
+      $('#appModal .modal-bio').text(bio);
+      $('#appModal .modal-link').text(link);
+      $('#appModal .modal-link').attr("href", link);
+      $('#appModal .modal-tech').text(technologies);
+
+      $('#appModal').modal();
+    }
+
+
+/*
+//multiple categories
+  $('.category').on('change', function() {
+    var category_filters = [];
+  
+    $('.search-filters select').each(function() {
+      if ($(this).val() != 0) {
+        category_filters[$(this).attr('id')] = $(this).val();
+      }
+    });
+  
+    $(".result-set .result").hide().filter(function() { 
+      var show = true;
+      for (var category in category_filters) {
+         show = show && $(this).data(category) == category_filters[category];
+      }
+  
+      return show;
+    }).show();
+  
+  });*/
